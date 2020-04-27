f1b9b6dc3d05b5ab7afa74dbf47782cb511a3854
RanggaReganansa <ranggareganansa@gmail.com>
2020-04-27 11:06:12 +0800

    DO WHILE

---
+++ src/main2.java
@@ -0,0 +1,17 @@
+public class main2 {
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi gelas sekarang : " + isiGelas + "ml");
+        System.out.println("isi gelas ketika penuh : " + penuh + "ml");
+
+        do {
+            isiGelas++;
+            System.out.println("Sedang mengisi gelas.. ");
+            System.out.println("Isi gelas sekarang : " + isiGelas + "ml");
+        } while (isiGelas != penuh)
+
+        System.out.println("Finale isi gelas sekarang : " + isiGelas + "ml");
+    }
+}
